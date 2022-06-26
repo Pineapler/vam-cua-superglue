bb7cb8d92d633c447465d7718bf5c1637fcafbf7
Pineapler <pineaplers@gmail.com>
2022-06-26 23:13:46 +1000

    Refactor, added usage and info

--- VamCuaSuperglue.cs
+++ VamCuaSuperglue.cs
@@ -1,110 +1,151 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
-using MeshVR;
-using PrefabEvolution;
 using UnityEngine;
 
-public class VamCuaSuperglue : MVRScript
-{
+public class VamCuaSuperglue : MVRScript {
     public List<Atom> cuaAtoms;
-    public List<GlueEntry> glueEntries;
-    public List<Pose> prevFrame;
-    public List<Pose> thisFrame;
+    public Dictionary<Atom, GlueEntry> glueEntries;
+    public UIDynamicTextField infoPanel;
+    public UIDynamicTextField usagePanel;
+    public JSONStorableString infoString;
+    public JSONStorableString usageString;
 
     public override void Init() {
-        // Subscribe to Atom delegates
-
         try {
+            InitCustomUI();
+            SubscribeDelegates();
             RefreshCuas();
         }
         catch (Exception e) {
             Err("[VamCuaSuperglue]: " + e);
-            DestroyImmediate(this);
+            Destroy(this);
         }
     }
 
+
+    public void InitCustomUI() {
+        infoString = new JSONStorableString("infoString", "Info");
+        infoPanel = CreateTextField(infoString);
+        infoPanel.height = 1000;
+
+        string tmp = @"CUA Superglue by Pineapler
+
+Usage:
+This plugin should work automatically, but in the event of it not working here are some options to troubleshoot.
+
+In your CUA, make sure the following options are selected:
+
+Physics Object
+    > Physics (Disabled)
+
+Control
+    > Link To Atom (As desired)
+    > Position, Rotation (Parent Link)";
+
+        usageString = new JSONStorableString("usageString", tmp);
+        usagePanel = CreateTextField(usageString, true);
+        usagePanel.height = 1000;
+    }
+
+
+    public void SubscribeDelegates() {
+        // SuperController.singleton.onAtomUIDsChangedHandlers += OnUIDsChanged;
+
+    }
+
+    public void UnsubscribeDelegates() {
+
+    }
+
     public void OnDestroy() {
-        // Unsubscribe from Atom delegates
+        UnsubscribeDelegates();
     }
 
 
     public void RefreshCuas() {
         cuaAtoms = SuperController.singleton.GetAtoms()
-            .FindAll(a => a.type.Equals("CustomUnityAsset") &&
-                          a.mainController.linkToRB != null);
+            .FindAll(a => a.type.Equals("CustomUnityAsset"));
 
-
-        prevFrame = new List<Pose>(cuaAtoms.Count);
-        thisFrame = new List<Pose>(cuaAtoms.Count);
-        glueEntries = new List<GlueEntry>(cuaAtoms.Count);
+        glueEntries = new Dictionary<Atom, GlueEntry>(cuaAtoms.Count);
 
         foreach (Atom a in cuaAtoms) {
-            Transform objTransform = a.transform.Find("reParentObject/object");
-
-            Pose pose = new Pose(objTransform.position, objTransform.rotation);
-            prevFrame.Add(pose);
-            thisFrame.Add(pose);
-
-            JSONStorable control = a.GetStorableByID("control");
-            // Log(a.name + " " + (a.mainController.linkToRB != null));
-            bool isPhysicsEnabled = control.GetBoolParamValue("physicsEnabled");
-            string posStr = control.GetStringChooserParamValue("positionState");
-            string rotStr = control.GetStringChooserParamValue("rotationState");
-            bool isPosParentLink = posStr != null && posStr.Equals("ParentLink");
-            bool isRotParentLink = rotStr != null && rotStr.Equals("ParentLink");
-            glueEntries.Add(new GlueEntry(objTransform, isPhysicsEnabled, isPosParentLink, isRotParentLink));
-
+            glueEntries.Add(a, new GlueEntry(a));
         }
-        // Log(cuaAtoms[2].GetStorableByID("control").GetAllParamAndActionNames().Aggregate( (current, next) => current + "\n    " + next));
+
+        UpdateInfoPanel();
     }
 
-    // public void OnUIDsChanged(List<string> uids) {
-    //     foreach (string s in uids) {
-    //         Log(s);
-    //     }
-    // }
+
+    public void UpdateInfoPanel() {
+        infoString.val = "Glued CUAs:\n";
+        StringBuilder builder = new StringBuilder();
+        foreach (KeyValuePair<Atom, GlueEntry> pair in glueEntries) {
+            bool isTracked = pair.Key.mainController.linkToRB != null &&
+                             !pair.Value.isPhysicsEnabled;
+            builder.Append(isTracked ? "\n[X]   " : "\n[   ]   ");
+            builder.Append(pair.Key.name);
+        }
+
+        infoString.val += builder.ToString();
+    }
 
 
     public void LateUpdate() {
-        for (int i = 0; i < glueEntries.Count; i++) {
-            GlueEntry g = glueEntries[i];
-            if (g.isPhysicsEnabled) {
-                continue;
+        try {
+            foreach (KeyValuePair<Atom, GlueEntry> pair in glueEntries) {
+                pair.Value.Stick();
             }
-
-            thisFrame[i] = new Pose(g.transform.position, g.transform.rotation);
-            Pose prevPose = prevFrame[i];
-
-            if (g.isPosParentLink) {
-                g.transform.position = prevPose.position;
-            }
-            if (g.isRotParentLink) {
-                g.transform.rotation = prevPose.rotation;
-            }
-
         }
-
-        // swap pose buffers
-        List<Pose> tempList = prevFrame;
-        prevFrame = thisFrame;
-        thisFrame = tempList;
+        catch (Exception e) {
+            Err("[VamCuaSuperglue]: " + e);
+            Destroy(this);
+        }
     }
 
     public class GlueEntry {
-        // public Atom atom;
         public Transform transform;
+        public Pose prevPose;
+        public Pose thisPose;
         public bool isPhysicsEnabled;
         public bool isPosParentLink;
         public bool isRotParentLink;
 
-        public GlueEntry(Transform transform = null, bool isPhysicsEnabled = false, bool isPosParentLink = true, bool isRotParentLink = true) {
-            this.transform = transform;
-            this.isPhysicsEnabled = isPhysicsEnabled;
-            this.isPosParentLink = isPosParentLink;
-            this.isRotParentLink = isRotParentLink;
+        public GlueEntry(Atom atom) {
+            RefreshGlue(atom);
+        }
+
+        public void RefreshGlue(Atom atom){
+            transform = atom.transform.Find("reParentObject/object");
+            JSONStorable control = atom.GetStorableByID("control");
+
+            isPhysicsEnabled = control.GetBoolParamValue("physicsEnabled");
+            string posStr = control.GetStringChooserParamValue("positionState");
+            string rotStr = control.GetStringChooserParamValue("rotationState");
+            isPosParentLink = posStr != null && posStr.Equals("ParentLink");
+            isRotParentLink = rotStr != null && rotStr.Equals("ParentLink");
+
+            if (transform != null) {
+                prevPose = new Pose(transform.position, transform.rotation);
+                thisPose = prevPose;
+            }
+
+        }
+
+        public void Stick() {
+            if (isPhysicsEnabled) return;
+
+            thisPose = new Pose(transform.position, transform.rotation);
+
+            if (isPosParentLink) {
+                transform.position = prevPose.position;
+            }
+            if (isRotParentLink) {
+                transform.rotation = prevPose.rotation;
+            }
+
+            prevPose = thisPose;
         }
     }
 
